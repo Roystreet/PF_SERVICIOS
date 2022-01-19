272f18add913b305b446b9ec79aa97aa8ff2c537
Alvaro Canales Aguilar <alvarocanales1599@gmail.com>
2022-01-19 16:14:37 -0500

    posts

--- Src/Controllers/PostControllers/index.js
+++ Src/Controllers/PostControllers/index.js
@@ -1,15 +1,42 @@
-const { post } = require("../../Models");
+const Post = require('../../Models/Post');
+const User = require('../../Models/User');
 
-const getPosts = async (req, res, next) => {};
-
-const getPostUsers = (req, res, next) => {};
+const getPosts = async (req, res, next) => {
+	try {
+		const dataFound = await Post.findAll({});
+		res.status(200).json(dataFound);
+		return;
+	} catch (error) {
+		res.status(500).json({ msg: [ error ] });
+		console.log('Error', Error);
+		return;
+	}
+};
+const getPostUsers = async (req, res, next) => {
+	const params = req.params;
+	try {
+		const dataFound = await Post.findAll({
+			include: {
+				model: User,
+				where: {
+					id: params
+				}
+			}
+		});
+		res.status(200).json(dataFound);
+		return;
+	} catch (error) {
+		res.status(500).json();
+		console.log('Error: ', Error);
+	}
+};
 const updatePosts = (req, res, next) => {};
 
 const deletePosts = (req, res, next) => {};
 
 module.exports = {
-  getPosts,
-  getPostUsers,
-  updatePosts,
-  deletePosts,
+	getPosts,
+	getPostUsers,
+	updatePosts,
+	deletePosts
 };
