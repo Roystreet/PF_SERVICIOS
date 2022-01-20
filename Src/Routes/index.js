b5ce4f11751843e5062ae0ab43d08cc8059cdb89
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2022-01-20 10:51:28 -0500

    confrilcts

--- Src/Routes/index.js
+++ Src/Routes/index.js
@@ -27,17 +27,7 @@ router.get("/post", getPosts);
 router.put("/post", updatePosts);
 router.get("/post/:user", getPostUsers);
 router.delete("/post/:user", deletePosts);
-<<<<<<< HEAD
 
-//products
-router.get('/products', getProducts)
-router.post('/product',postProduct)
-router.get('/products/:id',getProductById)
-router.put('/product',updateProduct)
-router.delete('/product/:id',deleteProduct)
-
-=======
->>>>>>> 6225b65f6afbe38a2cae67e07a2070696f5c98d8
 //Users
 router.post("/register", createUsers);
 router.get("/users", getUsers);
@@ -46,6 +36,7 @@ router.delete("/user/:id", deleteUser);
 router.post("/user/restore");
 router.put("/user/:id");
 router.post("/login");
+
 //Products
 router.get("/products?name");
 router.get("/products");
