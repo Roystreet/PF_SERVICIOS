4d14e69cc8cfa5df5b69d534a4b002efcfb24637
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-01-20 13:56:40 -0500

    cambio en la rama

--- Src/Models/Category.js
+++ Src/Models/Category.js
@@ -1,11 +1,13 @@
 const sequelize = require("../Database");
 const { DataTypes } = require("sequelize");
-const Product = require("./Product");
 
 const Category = sequelize.define("category", {
   name: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
 });
+
+// Relacion de categorias
 
 module.exports = Category;
