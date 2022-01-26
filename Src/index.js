5f99b576ef26aae38731f8abca109f88079ddc43
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2022-01-26 09:23:47 -0500

    update countries scripts

--- Src/index.js
+++ Src/index.js
@@ -5,9 +5,17 @@ app.listen(app.get("PORT"), async () => {
   try {
     await sequelize.authenticate();
     console.log("database is ready");
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: false });
     console.log("server on port " + app.get("PORT"));
   } catch (error) {
     console.log(error);
   }
 });
+
+async function paddingData() {
+  let con = await sequelize.models.Country.findAll()
+  if(con.length == 0){
+    require("../downloadCountries")
+  }
+}
+paddingData()
