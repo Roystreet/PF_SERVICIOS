e50498184a4f97746f56635f12abb54b9c3baebd
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-01-25 23:46:54 -0500

    se ha modalizado las rutas por completo, teniendo las rutas publicas, las de los usuario y la del admi para mas facil mantenabilidad

--- Src/Routes/admin.js
+++ Src/Routes/admin.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const {
-  getPosts,
   getPostUsers,
   updatePosts,
   deletePosts,
@@ -12,48 +11,30 @@ const {
   getUsersById,
   deleteUser,
   createUsers,
-  logIn,
   resetPasswordForce,
 } = require("../Controllers/UserControllers");
 const {
-  getCategories,
   postCategory,
   deleteCategory,
 } = require("../Controllers/CategoryControllers");
 const { getOrders, getOrderId } = require("../Controllers/OrderControllers");
-const { getCountries } = require("../Controllers/CountryControllers");
 
 // Route for Post
-router.get("/post?name", getPosts);
-router.get("/post", getPosts);
 router.put("/post", updatePosts);
-router.get("/postbyuser/:userId", getPostUsers);
-router.get("/posts/:id", getPostById);
 router.delete("/post/:id", deletePosts);
 router.post("/post", createPosts);
 // Route for Users
-router.post("/register", createUsers);
 router.get("/users", getUsers);
 router.get("/user/:id", getUsersById);
 router.delete("/user/:id", deleteUser);
-router.post("/user/reset-password-force", resetPasswordForce);
+router.put("/user/reset-password-force", resetPasswordForce);
 router.put("/user/:id");
-router.post("/login", logIn);
-
+router.post("/user", createUsers);
 // Route for Category
-
-router.get("/category", getCategories);
 router.post("/category", postCategory);
 router.delete("/category/:id", deleteCategory);
-
 // Route Orders
-
 router.get("/orders", getOrders);
 router.get("/order/:id", getOrderId);
-router.post("/order");
-
-// Route
-
-router.get("/countries", getCountries);
 
 module.exports = router;
