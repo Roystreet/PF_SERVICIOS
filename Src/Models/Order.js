f750a4251d1cb7b2435504bc76a30923ad9ce89e
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-01-26 20:40:28 -0500

    change previous in model s

--- Src/Models/Order.js
+++ Src/Models/Order.js
@@ -12,8 +12,8 @@ const Order = sequelize.define("Order", {
   },
 });
 
-Order.hasMany(User);
-User.belongsTo(Order, { through: "OrderUser" });
+User.hasMany(Order);
+Order.belongsTo(User);
 
 Order.belongsToMany(Post, { through: "OrderPost" });
 Post.belongsToMany(Order, { through: "OrderPost" });
