399dd8c64b498c5b9bd2402aac19c94dd36dd5ce
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-01-29 21:55:04 -0500

    se ha integrado mercado pago

---
+++ Src/Controllers/MercadoPagoController/index.js
@@ -0,0 +1,40 @@
+const mercadopago = require("mercadopago");
+
+mercadopago.configure({
+  access_token:
+    "TEST-8815536356433300-012714-798045927f1e8d0e17b1412b6152bf2c-653565141",
+});
+
+const createPreference = async (req, res) => {
+  const preference = {
+    items: [
+      {
+        title: "algo",
+        description: "algo",
+        unit_price: 20,
+        quantity: 20,
+      },
+    ],
+    back_urls: {
+      success: "http://localhost:4000/api/feedback",
+      failure: "http://localhost:4000/api/feedback",
+      pending: "http://localhost:4000/api/feedback",
+    },
+    auto_return: "approved",
+  };
+
+  const chargeData = await mercadopago.preferences.create(preference);
+  console.log(chargeData);
+  res.status(200).json({ res: chargeData.body.init_point });
+};
+
+const feedback = async (req, res) => {
+  const data = req.query;
+  console.log(data);
+  res.redirect("http://localhost:3001/");
+};
+
+module.exports = {
+  createPreference,
+  feedback,
+};
